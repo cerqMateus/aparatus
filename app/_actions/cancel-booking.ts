6362077ce254d37dd04dafd06a2ac6e94a312e0b
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-11-22 09:22:28 -0300

    chore: change api version to 2025-11-17.clover

--- app/_actions/cancel-booking.ts
+++ app/_actions/cancel-booking.ts
@@ -57,7 +57,7 @@ export const cancelBooking = actionClient
 
             const Stripe = (await import("stripe")).default;
             const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-                apiVersion: "2025-07-30.basil"
+                apiVersion: "2025-11-17.clover"
             });
 
             try {
